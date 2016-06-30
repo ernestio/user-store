1e686fb37eac2af1058db5fc54be9f41d3b818c1
tom <tom.bevan@r3labs.io>
2016-06-30 18:13:51 +0100

    reduced wait time on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Waiting for Postgres"
-while ! echo exit | nc postgres 5432; do sleep 10; done
+while ! echo exit | nc postgres 5432; do sleep 1; done
 
 echo "Starting user-store"
 /go/bin/user-store
